9f65bd865a694b8326ba502ded7083e998d1ffd5
yanfu <yanfu2727@gmail.com>
2019-03-15 11:47:57 +0800

    second commit

---
+++ app.js
@@ -0,0 +1,10 @@
+<HEAD>
+<script language="JavaScript" type="text/javascript">
+function changesize(){ 
+  window.resizeTo(400,250); //指定預開啟的寬度與高度
+} 
+window.onload=changesize; 
+window.onresize=changesize;
+</script>
+</HEAD>
+<BODY>
