35a3aeff1e4df8a164015b3490ecf0c1550a6e38
yanfu <yanfu2727@gmail.com>
2019-03-18 14:22:11 +0800

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-<HEAD>
-<script language="JavaScript" type="text/javascript">
-function changesize(){ 
-  window.resizeTo(400,250); //指定預開啟的寬度與高度
-} 
-window.onload=changesize; 
-window.onresize=changesize;
-</script>
-</HEAD>
-<BODY>
+setTimeout(function(){
+  $('#picture2').animate({marginLeft: '180px',marginTop:'-30px'},3000);
+  $('#picture3').animate({marginLeft: '180px'},3000);
+  $('h5').animate({opacity:'1'},3000,function(){
+    $('#picture2').animate({opacity:'0'},1000);
+    $('#picture3').animate({opacity:'0'},1000,function(){
+
+      $('#picture2_2').animate({opacity:'1'},1000);
+      $('#picture3_2').animate({opacity:'1'},1000,function(){
+        $('#picture2_2').animate({marginLeft:'-180px',marginTop:'-15px'},3000);
+        $('#picture3_2').animate({marginLeft:'-180px'},3000);
+        $('#h4-2').animate({opacity:'1'},3000,function(){
+          $('#picture2_2').animate({opacity:'0'},1000);
+          $('#picture3_2').animate({opacity:'0'},1000,function(){
+
+            $('#picture2_3').animate({opacity:'1'},1000);
+            $('#picture3_3').animate({opacity:'1'},1000,function(){
+              $('#picture2_3').animate({marginLeft:'180px',marginTop:'-15px'},3000);
+              $('#picture3_3').animate({marginLeft:'180px'},3000);
+              $('#h4-3').animate({opacity:'1'},3000,function(){
+                $('#picture2_3').animate({opacity:'0'},1000);
+                $('#picture3_3').animate({opacity:'0'},1000,function(){
+                  $('#picture2_4').animate({opacity:'1'},1000);
+                  $('#picture3_4').animate({opacity:'1'},1000,function(){
+                    $('#picture2_4').animate({marginLeft:'-180px',marginTop:'-30px'},3000);
+                    $('#picture3_4').animate({marginLeft:'-180px'},3000);
+                    $('#h4-4').animate({opacity:'1'},3000);
+                  });
+                });
+              });
+            });
+          });          
+        });
+      });
+    });
+  });
+})
+$('#menu').click(function(){
+  $('#fork').animate({opacity:'1'},500);
+  $('#knife').animate({opacity:'1'},500);
+})
+
